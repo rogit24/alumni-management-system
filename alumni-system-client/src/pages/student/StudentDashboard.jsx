ed0ddbc2a8c8e48a91231bc59ac29308e912cfd4
naveendandugula56 <naveendandugula56@gmail.com>
2026-06-06 19:40:27 +0530

    Studemt dashboard

--- alumni-system-client/src/pages/student/StudentDashboard.jsx
+++ alumni-system-client/src/pages/student/StudentDashboard.jsx
@@ -47,3 +47,94 @@ function StudentDashboard() {
       messages: messages.length,
     });
   }, []);
+
+   return (
+    <StudentLayout>
+      <div className="container-fluid">
+
+        {/* Welcome Section */}
+
+        <div className="mb-5">
+          <h2 className="fw-bold">
+            🎓 Student Dashboard
+          </h2>
+
+          <p className="text-light">
+            Welcome back, {userName}
+          </p>
+        </div>
+
+        {/* Dashboard Cards */}
+
+        <div className="row g-4">
+
+          <div className="col-md-3">
+            <div className="student-card student-card-blue p-4 position-relative">
+
+              <div className="card-icon">
+                💼
+              </div>
+
+              <h2>{stats.jobs}</h2>
+
+              <p>Available Jobs</p>
+
+            </div>
+          </div>
+
+          <div className="col-md-3">
+            <div className="student-card student-card-green p-4 position-relative">
+
+              <div className="card-icon">
+                📄
+              </div>
+
+              <h2>
+                {stats.applications}
+              </h2>
+
+              <p>Applications</p>
+
+            </div>
+          </div>
+
+          <div className="col-md-3">
+            <div className="student-card student-card-orange p-4 position-relative">
+
+              <div className="card-icon">
+                🤝
+              </div>
+
+              <h2>{stats.referrals}</h2>
+
+              <p>Referrals</p>
+
+            </div>
+          </div>
+
+          <div className="col-md-3">
+            <div className="student-card student-card-purple p-4 position-relative">
+
+              <div className="card-icon">
+                💬
+              </div>
+
+              <h2>{stats.messages}</h2>
+
+              <p>Messages</p>
+
+            </div>
+          </div>
+
+        </div>
+
+        {/* Quick Info Section */}
+
+     
+
+      </div>
+    </StudentLayout>
+  );
+}
+
+export default StudentDashboard;
