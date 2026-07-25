59853ffb63ac398f86090fcc5413fed5b4567271
naveendandugula56 <naveendandugula56@gmail.com>
2026-07-25 13:21:38 +0530

    Job-Microservice started

---
+++ JobMS/src/main/java/com/alumniconnect/job/JobApplication.java
@@ -0,0 +1,21 @@
+package com.alumniconnect.job;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class JobApplication {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobApplication.class, args);
+    }
+}
---
+++ alumni-system-client/src/pages/AuthPage.jsx
@@ -0,0 +1,7 @@
+import Login from './auth/Login';
+
+function AuthPage() {
+  return <Login />;
+}
+
+export default AuthPage;
