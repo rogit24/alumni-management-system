b9d48b8160ba97f30bca5e677fecdc8c9db93a67
naveendandugula56 <naveendandugula56@gmail.com>
2026-07-25 16:49:33 +0530

    Job Entity Created

---
+++ JobMS/src/main/java/com/alumniconnect/job/entity/Job.java
@@ -0,0 +1,43 @@
+package com.alumniconnect.job.entity;
+
+import java.time.LocalDate;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "jobs")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Job {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String company;
+
+    private String salary;
+
+    private String location;
+
+    @Column(length = 1000)
+    private String description;
+
+    private String postedByEmail;
+
+    private LocalDate postedByDate;
+}
